1695b6da7a05fcad74fab0926c28b5a3f1e79bd5
unknown <alizahra2403@gmail.com>
2024-12-06 09:03:41 +0700

    fix: revise footer based on feedback

--- app/dlor/layout.tsx
+++ app/dlor/layout.tsx
@@ -4,7 +4,7 @@ export default function DlorLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 ">
+    <section className="relative flex flex-col h-screen">
       <div className="inline-block max-w-7xl text-center pt-8 justify-center">
         {children}
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
           <div className="relative flex flex-col h-screen ">
             <Navbar />
-            <main className="container mx-auto max-w-7xl pt-3 px-6 flex-grow">
+            <main className="container mx-auto max-w-7xl pt-3 flex-grow">
               {children}
             </main>
           </div>
